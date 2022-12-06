c2e1c7d9ebfc36667f1bbaeb1c17a74fcf48c03c
Andrei Talpan <talpanandrei@gmail.com>
2022-12-06 13:00:59 +0000

    Update realtime_img.py

--- pages/realtime_img.py
+++ pages/realtime_img.py
@@ -1,6 +1,6 @@
 import pathlib
 from os.path import exists
-
+import numpy as np
 import streamlit as st
 import cv2
 
@@ -9,37 +9,34 @@ cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_
 clf = cv2.CascadeClassifier(str(cascade_path))
 
 st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
 
-btn = st.button("Capture face")
-
-camera = cv2.VideoCapture(0)
+img_file_buffer = st.camera_input("Take a picture")
 path = "./Images"
-while run:
-    _, frame = camera.read()
-    gray_img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    FRAME_WINDOW.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    if btn:
-        faces = clf.detectMultiScale(gray_img,
-                                     scaleFactor=1.1,
-                                     minNeighbors=5,
-                                     minSize=(30, 30),
-                                     flags=cv2.CASCADE_SCALE_IMAGE)
-        for (x, y, width, height) in faces:
-            cv2.rectangle(gray_img, (x, y), (x + width, y + height), (0, 255, 0), 2)
-            face_frame = gray_img[y:height + y, x:x + width]
-            cv2.imwrite(path+"/extracted_face.jpg", face_frame)
-        st.write("Extracted face is:")
-        if exists(path+"/extracted_face.jpg"):
-            st.image(cv2.imread(path+"/extracted_face.jpg"))
-        camera.release()
-        break
-else:
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-    st.write("Stopped")
-    if exists(path + "/extracted_face.jpg"):
-        st.write("Extracted face is:")
+    # Check the type of cv2_img:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(cv2_img))
+
+    # Check the shape of cv2_img:
+    # Should output shape: (height, width, channels)
+    st.write(cv2_img.shape)
+
+    gray_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(gray_img,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(30, 30),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, width, height) in faces:
+        cv2.rectangle(gray_img, (x, y), (x + width, y + height), (0, 255, 0), 2)
+        face_frame = gray_img[y:height + y, x:x + width]
+    cv2.imwrite(path + "/extracted_face.jpg", face_frame)
+    st.write("Extracted face is:")
+    if exists(path+"/extracted_face.jpg"):
         st.image(cv2.imread(path+"/extracted_face.jpg"))
 
 
