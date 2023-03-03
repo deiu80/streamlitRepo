a6b1bee5488e46dcc8e8617bbf9ab47d2cc0f11c
Andrei Talpan <talpanandrei@gmail.com>
2023-03-03 23:47:42 +0000

    added model comparison with deepface only and svm

--- pages/Image Uploader.py
+++ pages/Image Uploader.py
@@ -4,8 +4,8 @@ import streamlit as st
 from keras_preprocessing.image import load_img
 from PIL import Image
 import os
-from helpers import get_model_prediction, get_all_faces, image_resizer, face_detect_NN, get_marked_image, _max_width_
-from helpers import get_img_face_frame
+from helpers import get_model_prediction, get_all_faces, image_resizer, face_detect_NN, get_marked_image, _max_width_, \
+    labels
 from helpers import images_folder_path
 
 _max_width_()
@@ -43,31 +43,31 @@ if group_image_box:
         haar_column.subheader('HaarCascade classifier example')
 
         marked_image, nr_faces, faces_frames = get_marked_image(group_image_path)
-        haar_column.metric(label="Faces found",value=nr_faces)
+        haar_column.metric(label="Faces found", value=nr_faces)
         haar_column.image(marked_image)
 
         for el in faces_frames:
             gray_el = cv2.cvtColor(el, cv2.COLOR_RGB2GRAY)
             pil_image = Image.fromarray(gray_el)
             pil_image = pil_image.resize((48, 48))
-            emotion_predicted = get_model_prediction(pil_image)
+            emotion_predicted, scores = get_model_prediction(pil_image)
             haar_column.write("Model's prediction: " + emotion_predicted)
             haar_column.image(el)
 
-
         dnn_column.subheader("Classifier pre-trained on Res10 ")
-        annotated_img, nr_faces2 , faces_extracted = face_detect_NN(group_image_path, 0.7)
-        dnn_column.metric(label="Faces found",value=nr_faces2)
+        annotated_img, nr_faces2, faces_extracted = face_detect_NN(group_image_path, 0.7)
+        dnn_column.metric(label="Faces found", value=nr_faces2)
         dnn_column.image(annotated_img)
-        
         for el in faces_extracted:
-            gray_el = cv2.cvtColor(el,cv2.COLOR_RGB2GRAY)
+            gray_el = cv2.cvtColor(el, cv2.COLOR_RGB2GRAY)
             pil_image = Image.fromarray(gray_el)
             pil_image = pil_image.resize((48, 48))
-            emotion_predicted = get_model_prediction(pil_image)
+            emotion_predicted, scores = get_model_prediction(pil_image)
             dnn_column.write("Model's prediction: " + emotion_predicted)
             dnn_column.image(el)
-
+            expander2 = dnn_column.expander("See models probabilities")
+            for lab, score in zip(labels, scores):
+                expander2.metric(value=score, label=lab)
     else:
         st.write("You can also select an image from left bar")
 
@@ -83,20 +83,21 @@ if single_images_box:
         haar_column.subheader('HaarCascade classifier example')
         marked_image, nr_faces, faces_frames = get_marked_image(selected_image_path)
         haar_column.metric(label="Faces found", value=nr_faces)
-
         haar_column.image(marked_image)
 
         for el in faces_frames:
             gray_el = cv2.cvtColor(el, cv2.COLOR_RGB2GRAY)
             pil_image = Image.fromarray(gray_el)
             pil_image = pil_image.resize((48, 48))
-            emotion_predicted = get_model_prediction(pil_image)
+            emotion_predicted, scores = get_model_prediction(pil_image)
             haar_column.write("Model's prediction: " + emotion_predicted)
             haar_column.image(el)
+            expander = haar_column.expander("See explanation")
+            expander.write("classes probabilities")
+            expander.write(scores)
 
         dnn_column.subheader("Classifier pre-trained on Res10 ")
         annotated_img, nr_faces2, faces_extracted = face_detect_NN(selected_image_path, 0.7)
-
         dnn_column.metric(label="Faces found", value=nr_faces2)
         dnn_column.image(annotated_img)
 
@@ -104,16 +105,18 @@ if single_images_box:
             gray_el = cv2.cvtColor(el, cv2.COLOR_RGB2GRAY)
             pil_image = Image.fromarray(gray_el)
             pil_image = pil_image.resize((48, 48))
-            emotion_predicted = get_model_prediction(pil_image)
+            emotion_predicted, scores = get_model_prediction(pil_image)
             dnn_column.write("Model's prediction: " + emotion_predicted)
             dnn_column.image(el)
+
+
     else:
         st.write("You can also select an image from left bar")
 
 if file_uploaded is not None:
     st.title("Here is the original image you've uploaded")
     original_img = Image.open(file_uploaded)
-    uploaded_file_path=images_folder_path+"/original"+file_uploaded.name
+    uploaded_file_path = images_folder_path + "/original" + file_uploaded.name
     original_img.save(uploaded_file_path)
     file_uploaded.close()
 
@@ -128,88 +131,6 @@ if file_uploaded is not None:
     haar_column.image(marked_image)
 
     dnn_column.subheader("Classifier pre-trained on Res10 ")
-    annotated_img, nr_faces2, faces_extracted= face_detect_NN(uploaded_file_path, 0.7)
+    annotated_img, nr_faces2, faces_extracted = face_detect_NN(uploaded_file_path, 0.7)
     dnn_column.metric(label="Faces found", value=nr_faces2)
     dnn_column.image(annotated_img)
-
-
-
-
-# if selected_image_path != "NA" or group_image_path != "NA":
-#     haar_column, dnn_column = st.columns(2)
-#     with haar_column:
-#         st.header("HaarCascade classifier example")
-#         if single_images_box:
-#             marked_image = get_marked_image(selected_image_path)
-#             st.image(marked_image)
-#         # # PIL Image instance.
-#         # resized_face_img = load_img(images_folder_path + '/extracted_face.jpg', target_size=(48, 48),
-#         #                             color_mode="grayscale")
-#         # emotion_predicted = get_model_prediction(resized_face_img)
-#         # st.write("Model's prediction: " + emotion_predicted)
-#     with dnn_column:
-#         st.header("DNN using res10 example")
-#         if group_images_box:
-#             marked_image2 = face_detect_NN(group_image_path, 0.7)
-#             st.image(marked_image2)
-#         # for face in all_faces:
-#         #     col1, col2 = st.columns(2)
-#         #     with col1:
-#         #         st.image(image_resizer(face, 100))
-#         #     with col2:
-#         #         pil_image = Image.fromarray(face)
-#         #         pil_image = pil_image.resize((48, 48))
-#         #         emotion_predicted = get_model_prediction(pil_image)
-#         #         st.write("Model's prediction: " + emotion_predicted)
-
-# if file_uploaded is not None:
-#     original_img = Image.open(file_uploaded)
-#     original_img.save("./original.jpg")
-#     file_uploaded.close()
-# else:
-#     st.write("No image selected")
-# if selected_image_path != "NA":
-#     # original image
-#     st.title("Here is the image you've selected")
-#     img = cv2.imread(selected_image_path)
-#     with st.container():
-#         all_faces = get_all_faces(selected_image_path)
-#         st.image(face_detect_NN(img))
-#
-#         for face in all_faces:
-#             col1, col2 = st.columns(2)
-#             with col1:
-#                 st.image(image_resizer(face, 100))
-#             with col2:
-#                 pil_image = Image.fromarray(face)
-#                 pil_image = pil_image.resize((48, 48))
-#                 emotion_predicted = get_model_prediction(pil_image)
-#                 st.write("Model's prediction: " + emotion_predicted)
-#
-#     if len(all_faces) < 2:
-#         # extracting the face from image
-#         face_frame = get_img_face_frame(selected_image_path)
-#         st.write("Extracted face is: ")
-#         st.image(face_frame)
-#
-#     # PIL Image instance.
-#     resized_face_img = load_img(images_folder_path + '/extracted_face.jpg', target_size=(48, 48),
-#                                 color_mode="grayscale")
-#     emotion_predicted = get_model_prediction(resized_face_img)
-#     st.write("Model's prediction: " + emotion_predicted)
-# else:
-#     if file_uploaded is not None:
-#         # get original image
-#         #  get face from the image, scale it to 48*48 and convert it to grayscale
-#         st.title("Here is the image you've uploaded")
-#         img = cv2.imread("./original.jpg")
-#         st.image(img)
-#         # extracting the face from image
-#         face_frame = get_img_face_frame("./original.jpg")
-#         st.write("Extracted face is: ")
-#         st.image(face_frame)
-#
-#         resized_face_img = load_img(images_folder_path + '/extracted_face.jpg', target_size=(48, 48),
-#                                     color_mode="grayscale")
-#         emotion_predicted = get_model_prediction(resized_face_img)
-#         st.write("Model's prediction: " + emotion_predicted)
--- pages/Webcam capture.py
+++ pages/Webcam capture.py
@@ -10,23 +10,22 @@ st.set_page_config(
     page_title="CE301 - Webcam Live Feed"
 )
 
-classifier = get_classifier()
 st.title("Webcam Live Feed")
 
 img_file_buffer = st.camera_input("Take a frontal picture")
 path = "./Images"
-
 
 if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
     gray_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
-    faces = classifier.detectMultiScale(gray_img,
-                                 scaleFactor=1.1,
-                                 minNeighbors=5,
-                                 minSize=(30, 30),
-                                 flags=cv2.CASCADE_SCALE_IMAGE)
+
+    faces = get_classifier().detectMultiScale(gray_img,
+                                        scaleFactor=1.1,
+                                        minNeighbors=5,
+                                        minSize=(30, 30),
+                                        flags=cv2.CASCADE_SCALE_IMAGE)
     for (x, y, width, height) in faces:
         cv2.rectangle(gray_img, (x, y), (x + width, y + height), (0, 255, 0), 2)
         face_frame = gray_img[y:height + y, x:x + width]
@@ -41,5 +40,8 @@ if img_file_buffer is not None:
         resized_img = load_img(path + "/extracted_face.jpg", target_size=(48, 48), color_mode="grayscale")
 
         enhanced_image = load_img(path + "/enhanced_face.jpg", target_size=(48, 48), color_mode="grayscale")
-        emotion_predicted = get_model_prediction(enhanced_image)
-        st.write("Model's prediction: " + emotion_predicted)
+        emotion_predicted, scores = get_model_prediction(enhanced_image)
+        st.write("Model's prediction: ")
+        st.write(emotion_predicted)
+        st.write(scores)
+#         TODO:change this to the new format with multiple libraries
