32081268b67e4f19f86a4371e35b80762e0b5bba
andrei <talpanandrei@gmail.com>
2023-04-23 19:07:25 +0100

    add info section and rename dropdown options

--- pages/Compare Classifiers.py
+++ pages/Compare Classifiers.py
@@ -9,71 +9,77 @@ from helpers import get_marked_image, emotion_labels, get_prediction_of_own_CNN,
     setup_svm
 from helpers import svm_get_predict, svm_model_exists, image_resizer
 
-all_images = ["NA"]
+my_images = []
 group_images = ["NA"]
 files = os.listdir(images_folder_path)
 st.set_page_config(layout="wide")
 svm_model_aws = setup_svm()
 
-for file in files:
-    # make sure file is an image
-    if file.endswith(('.jpg', '.png', 'jpeg')):
-        img_path = images_folder_path + "/" + file
-        if file.startswith("group"):
-            group_images.append(img_path)
-        else:
-            all_images.append(img_path)
 
+@st.cache_data
+def populate_dropdown():
+    paths_dict = {"Group 1": "",
+                  "Group 2": "",
+                  "Group 3": "",
+                  "Group 4": "",
+                  "Group 5": "",
+                  "Group 6": "",
+                  "Group 7": ""}
+    idx = 1
+    for file in files:
+        # make sure file is an image
+        if file.endswith(('.jpg', '.png', 'jpeg')):
+            img_path = images_folder_path + "/" + file
+            if file.startswith("group"):
+                group_images.append(img_path)
+                paths_dict["Group " + str(idx)] = img_path
+                idx += 1
+    return paths_dict
+
+
+paths = populate_dropdown()
 group_tab, uploader_tab = st.tabs(["Group Images", "Upload your own image"])
 
 
 def format_dictionary_probs(analysis):
     for k in analysis:
-        analysis[k] = round(analysis[k], 5)
+        analysis[k] = round(analysis[k], 3)
     return analysis
 
 
 def get_dictonary_probs(probabilities):
     obj = {}
     for i, emotion_label in enumerate(emotion_labels):
-        obj[emotion_label] = round(100 * probabilities[i], 5)
-    return obj
-
-
-@st.cache_data
-def get_dictionary_probs_RMN(results):
-    obj = {}
-    for record in results[0]['proba_list']:
-        for label in record:
-            obj[label] = round(record[label] * 100, 5)
-
+        obj[emotion_label] = round(100 * probabilities[i], 3)
     return obj
 
 
 with group_tab:
-    group_image_path = st.selectbox(
+    group = st.selectbox(
         'Select an image:',
-        group_images)
+        paths.keys())
 
-    if group_image_path != "NA":
+    if group != "NA":
         st.title("Here is the image you've selected")
-        st.write(
-            'Notice how the 2 face detection classifiers perform. On the right the neural network trained classifier is much better at finding faces')
+
+        st.write('Designed in 2001, HaarCascade is a very popular algorithm for face detection due to its speed and low computational requirements.')
+        st.write('But, as you can notice below it does produce false-positives and is not able to recognize all faces.')
+        st.divider()
 
         haar_column, dnn_column = st.columns(2)
         haar_column.subheader('HaarCascade face classifier')
 
-        marked_image, nr_faces, faces_frames = get_marked_image(group_image_path)
+        marked_image, nr_faces, faces_frames = get_marked_image(paths[group])
         haar_column.metric(label="Faces found", value=nr_faces)
         haar_column.image(marked_image)
 
-        dnn_column.subheader("neural network Face detector ")
+        dnn_column.subheader("Face detector based on CNN")
 
         detector = MTCNN()
 
-        img = cv2.imread(group_image_path)
+        img = cv2.imread(paths[group])
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        original_img = cv2.imread(group_image_path)
+        original_img = cv2.imread(paths[group])
         original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
 
         result = detector.detect_faces(img)
@@ -88,7 +94,13 @@ with group_tab:
         dnn_column.image(img)
 
         face_idx = 0
-        btn = dnn_column.button("Calculate confidence scores", key=face_idx)
+
+        st.subheader("Faces extracted")
+        btn = st.button("Get models predictions", key=face_idx)
+        st.info("Values in brackets is model's output.  \nIt can be interpreted as the confidence level of the model's prediction."
+                "  \nA higher probability :arrow_up: for a certain class means that the model is more confident that the input image belongs to that class. "
+                ,icon= "ℹ️")
+
         for face in faces_extracted:
             obj, dominant_emotion = get_prediction_of_own_CNN(face)
 
@@ -103,7 +115,7 @@ with group_tab:
 
                 col1, col2, col3 = expander2.columns(3)
                 col1.subheader("Using own CNN model")
-                col1.write('Dominant emotion ' + str(dominant_emotion))
+                col1.write('Dominant emotion: ' + str(dominant_emotion))
                 col1.write(obj)
 
                 if face_idx <= len(faces_extracted):
@@ -113,14 +125,14 @@ with group_tab:
                                                      enforce_detection=False)
                     face_analysis[0]['emotion'] = format_dictionary_probs(face_analysis[0]['emotion'])
 
-                    col2.write('Dominant emotion ' + str(face_analysis[0]['dominant_emotion']))
+                    col2.write('Dominant emotion: ' + str(face_analysis[0]['dominant_emotion']))
                     col2.write(face_analysis[0]['emotion'])
                     if svm_model_exists():
                         predicted_class, probabilities = svm_get_predict(svm_model_aws, face_img=face)
 
                         col3.subheader("Using SVM")
                         svm_dictionary = get_dictonary_probs(probabilities)
-                        col3.write('Dominant emotion ' + str(predicted_class))
+                        col3.write('Dominant emotion: ' + str(predicted_class))
                         col3.write(svm_dictionary)
 
                 face_idx += 1
@@ -133,6 +145,7 @@ with uploader_tab:
     if file_uploaded is not None:
         bytes_data = file_uploaded.getvalue()
         original_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
         if 'image' not in file_uploaded.type:
             st.warning("Please upload a JPG or PNG file.")
         else:
@@ -171,7 +184,7 @@ with uploader_tab:
 
                     col1.subheader("Using own model")
 
-                    col1.write(dominant_emotion)
+                    col1.write('Dominant emotion: '+dominant_emotion)
                     col1.write(obj)
                     # SVM
                     if svm_model_exists():
